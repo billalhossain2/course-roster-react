c1696aae7c0403671006346223894b757aa85ecb
billal-webdev <billal-webdev@gmail.com>
2023-09-15 11:03:31 +0600

    functionality added to calculate total credit hour

--- src/components/SelectedCourses/SelectedCourses.jsx
+++ src/components/SelectedCourses/SelectedCourses.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 const SelectedCourses = ({selectedCourses, creditHour}) => {
+  const getTotalCreditHours = selectedCourses.reduce((total, curr)=> total + curr.credit, 0)
   return (
     <div className='p-5 bg-[#f5f5f5] space-y-3'>
       <h3 className='text-[#2F80ED] font-bold text-[18px]'>Credit Hour Remaining {creditHour} hr</h3>
@@ -13,7 +14,7 @@ const SelectedCourses = ({selectedCourses, creditHour}) => {
       </ul>
       </div>
       <hr />
-      <h3 className='font-bold'>Total Credit Hour : 13</h3>
+      <h3 className='font-bold'>Total Credit Hour : {getTotalCreditHours}</h3>
       <hr />
       <h3 className='font-bold'>Total Price : 48000 USD</h3>
     </div>
